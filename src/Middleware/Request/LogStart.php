dc7e6858c5694e99540dd4f191d6589043a232e9
Mark Armendariz <src@enobrev.com>
2018-09-01 00:22:29 -0500

    Added LogStart Middleware to get logs working for API requests

---
+++ src/Middleware/Request/LogStart.php
@@ -0,0 +1,30 @@
+<?php
+    namespace Enobrev\API\Middleware\Request;
+
+    use Psr\Http\Message\ResponseInterface;
+    use Psr\Http\Message\ServerRequestInterface;
+    use Psr\Http\Server\MiddlewareInterface;
+    use Psr\Http\Server\RequestHandlerInterface;
+
+    use Enobrev\API\Middleware\FastRoute;
+    use Enobrev\API\RequestAttribute;
+    use Enobrev\API\RequestAttributeInterface;
+    use Enobrev\API\Spec;
+    use Enobrev\API\SpecInterface;
+    use Enobrev\Log;
+
+    use function Enobrev\dbg;
+
+    class LogStart implements MiddlewareInterface {
+        public function process(ServerRequestInterface $oRequest, RequestHandlerInterface $oHandler): ResponseInterface {
+            Log::i('Enobrev.Middleware.LogStart', [
+                '#request' => [
+                    'method'     => $oRequest->getMethod(),
+                    'path'       => $oRequest->getUri()->getPath(),
+                    'query'      => json_encode($oRequest->getQueryParams())
+                ]
+            ]);
+
+            return $oHandler->handle($oRequest);
+        }
+    }
