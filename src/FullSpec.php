90fb9a39b312f7ffbba200e64c4107b1eec854c1
Mark Armendariz <src@enobrev.com>
2018-09-19 08:08:15 -0500

    Opening Chmod on cache file

--- src/FullSpec.php
+++ src/FullSpec.php
@@ -10,6 +10,7 @@
     use Enobrev\API\Spec\ServerErrorResponse;
     use Enobrev\API\Spec\ValidationErrorResponse;
     use function Enobrev\dbg;
+    use Enobrev\Log;
     use RecursiveIteratorIterator;
     use RecursiveDirectoryIterator;
     use FilesystemIterator;
@@ -196,7 +197,11 @@
             $oFullSpec = new self;
             $oFullSpec->generateData();
             file_put_contents(self::$sPathToSpec, serialize($oFullSpec));
-            chmod(self::$sPathToSpec, 755);
+            try {
+                chmod(self::$sPathToSpec, 0777);
+            } catch (\Exception $e) {
+                Log::e('FullSpec.generateAndCache.chmodFail', ['error' => $e]);
+            }
             return $oFullSpec;
         }
 
