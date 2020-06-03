c28304bad0e598c3580af2f3d0ee59e2eed1ea67
Mark Armendariz <src@enobrev.com>
2020-06-02 23:03:07 -0500

    Handling bug in php that does not allow colons in relative urls, even when they should be allowed according to spec

--- src/Middleware/MultiEndpointPost.php
+++ src/Middleware/MultiEndpointPost.php
@@ -3,6 +3,7 @@
 
     namespace Enobrev\API\Middleware;
 
+    use Laminas\Diactoros\Exception\InvalidArgumentException;
     use RuntimeException;
 
     use Adbar\Dot;
@@ -64,12 +65,25 @@
                         $sEscaped = '(escaped): ' . str_replace('.', '+', $sEndpoint);
                     }
 
-                    $oUri         = new Uri($sEndpoint);
                     $aQueryParams = [];
-                    parse_str($oUri->getQuery(), $aQueryParams);
+                    try {
+                        $oUri = new Uri($sEndpoint);
+                        parse_str($oUri->getQuery(), $aQueryParams);
+                    } catch (InvalidArgumentException $e) {
+                        // parse_url does not like colons in urls
+                        // https://github.com/guzzle/guzzle/issues/1550
+                        // https://bugs.php.net/bug.php?id=71646
+                        if (strpos($sEndpoint, '?') === false) {
+                            $sEndpoint .= '?';
+                        } else {
+                            $sEndpoint = 'http://localhost/' . ltrim($sEndpoint, '/');
+                        }
+
+                        $oUri = new Uri($sEndpoint);
+                        parse_str($oUri->getQuery(), $aQueryParams);
+                    }
 
                     $aPostParams  = isset($aPost[$sEndpoint]) ? $this->fillPostTemplateFromData($aPost[$sEndpoint]) : [];
-
                     $oSubRequest  = ServerRequestFactory::fromGlobals()->withMethod(Method\POST)
                                                                        ->withUri($oUri)
                                                                        ->withQueryParams($aQueryParams)
--- src/Middleware/MultiEndpointQuery.php
+++ src/Middleware/MultiEndpointQuery.php
@@ -4,6 +4,7 @@
     namespace Enobrev\API\Middleware;
 
     use Countable;
+    use Laminas\Diactoros\Exception\InvalidArgumentException;
     use RuntimeException;
 
     use Adbar\Dot;
@@ -65,9 +66,24 @@
                         $sEscaped = '(escaped): ' . str_replace('.', '+', $sEndpoint);
                     }
 
-                    $oUri         = new Uri($sEndpoint);
                     $aQueryParams = [];
-                    parse_str($oUri->getQuery(), $aQueryParams);
+                    try {
+                        $oUri = new Uri($sEndpoint);
+                        parse_str($oUri->getQuery(), $aQueryParams);
+                    } catch (InvalidArgumentException $e) {
+                        // parse_url does not like colons in urls
+                        // https://github.com/guzzle/guzzle/issues/1550
+                        // https://bugs.php.net/bug.php?id=71646
+                        if (strpos($sEndpoint, '?') === false) {
+                            $sEndpoint .= '?';
+                        } else {
+                            $sEndpoint = 'http://localhost/' . ltrim($sEndpoint, '/');
+                        }
+
+                        $oUri = new Uri($sEndpoint);
+                        parse_str($oUri->getQuery(), $aQueryParams);
+                    }
+
                     $oSubRequest  = ServerRequestFactory::fromGlobals()->withMethod(Method\GET)
                                                                        ->withUri($oUri)
                                                                        ->withQueryParams($aQueryParams)
